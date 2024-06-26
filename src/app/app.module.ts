14834d92b3bbae757774a40eafbfebd751e28db8
Paulo Raimundo <pauloraimundo@paulo.raimundo>
2024-06-26 13:12:21 -0300

    Estruturando o app.module e o modulo de rotas.

---
+++ src/app/app.module.ts
@@ -0,0 +1,25 @@
+// src/app/app.module.ts
+
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
+import { ReactiveFormsModule } from '@angular/forms';
+
+// Componentes
+
+@NgModule({
+  declarations: [
+    AppComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    ReactiveFormsModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }

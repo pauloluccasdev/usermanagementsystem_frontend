6c01b726b0307784ba32dc7bb4a683fe1891e679
Paulo Raimundo <pauloraimundo@paulo.raimundo>
2024-06-26 20:25:41 -0300

    Implementando a exclusão de usuários.

--- src/app/components/user-list/user-list.component.ts
+++ src/app/components/user-list/user-list.component.ts
@@ -2,6 +2,8 @@
 import { Component, OnInit } from '@angular/core';
 import { User } from '../../models/user';
 import { UserService } from '../../service/user.service';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-user-list',
@@ -12,11 +14,34 @@ export class UserListComponent implements OnInit {
   users: User[] = [];
   displayedColumns: string[] = ['name', 'email', 'actions'];
 
-  constructor(private userService: UserService) { }
+  constructor(
+    private userService: UserService,
+    private snackBar: MatSnackBar,
+    private router: Router,
+  ) { }
 
   ngOnInit(): void {
+    this.loadUsers();
+  }
+  loadUsers() {
     this.userService.getUsers().subscribe(users => {
       this.users = users;
     });
+  }
+
+  deleteUser(id: number) {
+    this.userService.deleteUser(id).subscribe({
+      next:() => {
+        this.snackBar.open('User deleted successfully.', 'Close', {
+          duration: 3000,
+        });
+        this.loadUsers();
+      },
+      error:() => {
+        this.snackBar.open('Error deleting user.', 'Close', {
+          duration: 3000,
+        });
+      },
+    })
   }
 }
